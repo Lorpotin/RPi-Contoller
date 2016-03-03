3beac379635c213966e5a201f1224f9b6f25590f
Otto Laitinen <lorpotin@hotmail.com>
2016-03-03 03:08:36 +0200

    css tweaks

--- client/VideoScreen.jsx
+++ client/VideoScreen.jsx
@@ -20,9 +20,9 @@ var VideoScreen = React.createClass({
 		var srtData = "http://192.168.0.193:8000/video/"+data+".vtt";
 		data = "http://192.168.0.193:8000/video/"+data;
 		$("#video").remove();
-		$("#videoDiv").append("<video id='video' controls><source src='" + data + "'type=video/mp4></source><track id='track' src='"+srtData+"' kind='subtitle' srclang='en-US' label='English' /></video>" );
-		
-
+		var html = "<video id='video' controls preload='metadata'><source src='" + data + "'type=video/mp4></source><track id='track' src='"+srtData+"' kind='subtitle' srclang='en-US' label='English' /></video>";
+		$(html).hide().appendTo("#videoDiv").fadeIn(1500)
+	
 	},
 
 	showData: function() {
@@ -45,7 +45,7 @@ var VideoScreen = React.createClass({
 		promise = Promise.all(ajaxes).then(() => {
 			for(var i = 0; i < vd_data.length; i++) {
 				content.push(
-					<a id="videoBtn" name={vd_data[i].data} onClick={this.changeSrc.bind(null, vd_data[i].data)} className="waves-effect waves-light btn-large"><i className="material-icons right">cloud</i>{vd_data[i].data.split(".mp4")[0]}</a>		  
+					<a id="videoBtn" name={vd_data[i].data} onClick={this.changeSrc.bind(null, vd_data[i].data)} className="waves-effect waves-light btn-large"><i className="material-icons right">play_arrow</i>{vd_data[i].data.split(".mp4")[0]}</a>		  
 				);
 			}
 			
